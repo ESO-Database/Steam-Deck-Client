dafc441a9ada49015dcc57b92b5d11d2d04c2608
ESO-Database <github@eso-database.com>
2023-05-26 11:14:35 +0200

    Refactor update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,12 +4,14 @@ source /home/deck/Applications/ESO-Database/config/config.sh
 source /home/deck/Applications/ESO-Database/scripts/tools/functions.sh
 
 
-UPDATE_TEMP_DIR=/tmp/esodb_client_update
-ESODB_DESKTOP_APPLICATION_PATH="/home/deck/.local/share/applications"
 RELEASE_JSON=$(curl --location --silent ${ESODB_GITHUB_URL})
 REMOTE_VERSION="$(echo "${RELEASE_JSON}" | jq --raw-output '.tag_name')"
 LOCAL_VERSION=$(cat /home/deck/Applications/ESO-Database/version.txt | xargs) # xargs trims the string
 DOWNLOAD_URL="$(echo "${RELEASE_JSON}" | jq -r '.assets[].browser_download_url')"
+UPDATE_TEMP_DIR="/tmp/esodb_client_update"
+ESODB_DESKTOP_APPLICATION_PATH="/home/deck/.local/share/applications"
+ESODB_APPLICATION_PATH="/home/deck/Applications/ESO-Database"
+
 
 if [ ! "${LOCAL_VERSION}" = "${REMOTE_VERSION}" ]; then
 
@@ -37,7 +39,7 @@ if [ ! "${LOCAL_VERSION}" = "${REMOTE_VERSION}" ]; then
 
 
 	print_status "Applying update files..."
-	rsync --exclude="user.config.sh" --recursive --delete --human-readable "${UPDATE_TEMP_DIR}/" /home/deck/Applications/ESO-Database
+	rsync --exclude="user.config.sh" --recursive --delete --human-readable "${UPDATE_TEMP_DIR}/" "${ESODB_APPLICATION_PATH}"
 	print_success "Done"
 
 
@@ -47,30 +49,19 @@ if [ ! "${LOCAL_VERSION}" = "${REMOTE_VERSION}" ]; then
 
 
   print_status "Set script permissions..."
-  chmod +x /home/deck/Applications/ESO-Database/scripts/*.sh
+  chmod +x "${ESODB_APPLICATION_PATH}/scripts/"*.sh
   print_success "Done"
 
 
-	# TODO: Check if this step is required for the update-addons.sh script
-
-  print_status "Fetching Elder Scrolls Online AddOns meta data..."
-  fetch_addons_result=$(/home/deck/Applications/ESO-Database/scripts/api/get-addons.sh)
-  if [ "${fetch_addons_result}" = "ok" ]; then
-  	print_success "OK"
-  else
-  	print_error "Could not get AddOns meta information!"
-  fi
-
-
   print_status "Updating Elder Scrolls Online AddOns..."
-  /home/deck/Applications/ESO-Database/scripts/update-addons.sh
+  eval "${ESODB_APPLICATION_PATH}/scripts/update-addons.sh"
   print_success "Done"
 
 
   print_status "Updating background services"
-	cp -f /home/deck/Applications/ESO-Database/install/systemd/eso-database-uploader.service /home/deck/.config/systemd/user/eso-database-uploader.service
-  cp -f /home/deck/Applications/ESO-Database/install/systemd/eso-database-addon-updater.service /home/deck/.config/systemd/user/eso-database-addon-updater.service
-  cp -f /home/deck/Applications/ESO-Database/install/systemd/eso-database-addon-updater.timer /home/deck/.config/systemd/user/eso-database-addon-updater.timer
+  cp -f "${ESODB_APPLICATION_PATH}/install/systemd/eso-database-uploader.service" /home/deck/.config/systemd/user/eso-database-uploader.service
+  cp -f "${ESODB_APPLICATION_PATH}/install/systemd/eso-database-addon-updater.service" /home/deck/.config/systemd/user/eso-database-addon-updater.service
+  cp -f "${ESODB_APPLICATION_PATH}/install/systemd/eso-database-addon-updater.timer" /home/deck/.config/systemd/user/eso-database-addon-updater.timer
 
   systemctl --user daemon-reload
   systemctl --user try-restart eso-database-uploader.service
@@ -79,16 +70,16 @@ if [ ! "${LOCAL_VERSION}" = "${REMOTE_VERSION}" ]; then
 
 	print_status "Updating Launcher entries"
 	rm -f $(ls -d ${ESODB_DESKTOP_APPLICATION_PATH}/ESODB-Client-*)
-  cp -f /home/deck/Applications/ESO-Database/install/desktop/* "${ESODB_DESKTOP_APPLICATION_PATH}"
+  cp -f "${ESODB_APPLICATION_PATH}/install/desktop/"* "${ESODB_DESKTOP_APPLICATION_PATH}"
   print_success "Done"
 
 
 	print_status "Remove remaining setup files..."
-  rm -fr /home/deck/Applications/ESO-Database/install
+  rm -fr "${ESODB_APPLICATION_PATH}/install"
   print_success "Done"
 
 
 	print_success "The update to version ${REMOTE_VERSION} has been successfully performed!"
 else
-	print_status "You have the latest version installed!"
+	print_status "You have the latest version installed! (Version ${LOCAL_VERSION})"
 fi
